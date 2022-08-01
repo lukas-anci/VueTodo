6d11335c55463ba2d49104e5205879384b01e664
Lukas <lukas.ancikevicius@gmail.com>
2022-08-01 15:37:14 +0200

    Add an avatar

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer v-model="drawer" app>
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="title"> Vuetify Todo </v-list-item-title>
-          <v-list-item-subtitle> Best Todo Ever! </v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-
-      <v-divider></v-divider>
+    <v-navigation-drawer v-model="drawer" :mobile-breakpoint="768" app>
+      <v-img
+        class="pa-4 pt-7"
+        src="../src/assets/mountains.jpeg"
+        height="170"
+        gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
+      >
+        <v-avatar size="70" class="mb-2">
+          <img src="../src/assets/avatar.jpg" alt="Lukas" />
+        </v-avatar>
+        <div class="white--text text-subtitle-1 font-weight-bold">Bla</div>
+        <div class="white--text text-subtitle-2">Bla_bla</div>
+      </v-img>
 
       <v-list dense nav>
         <v-list-item v-for="item in items" :key="item.title" :to="item.to" link>
@@ -38,7 +42,7 @@
         ></v-img>
       </template>
 
-      <v-container class="pa-0">
+      <v-container class="header-container pa-0">
         <v-row>
           <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
           <v-spacer></v-spacer>
@@ -73,3 +77,7 @@ export default {
   components: { Snackbar, Search, LiveDateTime },
 };
 </script>
+<style lang="sass">
+.header-container
+   max-width:none
+</style>
